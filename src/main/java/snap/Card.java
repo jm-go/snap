29fdd1abd1a5a342161f56e9fa1c1fdfbaff7118
Jowita Gogol <jmg88@bath.ac.uk>
2024-02-01 15:56:46 +0000

    Added initial structure to Card class

---
+++ src/main/java/snap/Card.java
@@ -0,0 +1,24 @@
+package snap;
+
+public class Card {
+
+    private String suit;
+    private String symbol;
+    private int value;
+
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", value=" + value +
+                '}';
+    }
+
+}
