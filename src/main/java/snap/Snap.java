13ba1079a27b029b42cde7ac7c4879b8046770ab
Jowita Gogol <jmg88@bath.ac.uk>
2024-02-05 00:30:30 +0000

    Added playDuoGame() method to Snap class

--- src/main/java/snap/Snap.java
+++ src/main/java/snap/Snap.java
@@ -6,13 +6,15 @@ import java.util.Scanner;
 public class Snap extends CardGame {
 
     private Card previousCard;
+    private Player playerOne;
+    private Player playerTwo;
 
     public Snap(String name) {
         super(name);
         this.shuffleDeck();
     }
 
-    public void playGame() {
+    public void playAloneGame() {
         Scanner scanner = new Scanner(System.in);
         boolean isGameOn = true;
 
@@ -32,6 +34,42 @@ public class Snap extends CardGame {
             if (previousCard != null && previousCard.getSymbol().equals(dealtCard.getSymbol())) {
                 System.out.println("Snap! You win!\n");
                 isGameOn = false;
+            }
+
+            previousCard = dealtCard;
+        }
+    }
+
+    public void playDuoGame() {
+        this.playerOne = new Player("");
+        this.playerTwo = new Player("");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean isGameOn = true;
+        Player currentPlayer = playerOne;
+
+        System.out.print("Please enter a name for Player 1: ");
+        this.playerOne.setName(scanner.nextLine());
+        System.out.print("\nPlease enter a name for Player 2: ");
+        this.playerTwo.setName(scanner.nextLine());
+        while (isGameOn) {
+            System.out.println("\n" + currentPlayer.getName() + "'s turn. Press 'Enter' to draw a card.");
+            scanner.nextLine();
+
+            Optional<Card> nextCard = this.dealCard();
+            if (nextCard.isEmpty()) {
+                System.out.println("\nThe card deck is now empty. Game over.\n");
+                break;
+            }
+
+            Card dealtCard = nextCard.get();
+            System.out.println(currentPlayer.getName() + " drew: \n\n" + dealtCard);
+
+            if (previousCard != null && previousCard.getSymbol().equals(dealtCard.getSymbol())) {
+                System.out.println("\nSnap! " + currentPlayer.getName() + " wins!\n");
+                isGameOn = false;
+            } else {
+                currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
             }
 
             previousCard = dealtCard;
